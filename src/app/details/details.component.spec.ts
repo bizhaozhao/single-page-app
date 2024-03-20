7757dc70d37bd5091b61f2c3488f68fb3cd15609
Samuel Garratt <>
2024-03-21 07:58:51 +1300

    Fix tests

--- src/app/details/details.component.spec.ts
+++ src/app/details/details.component.spec.ts
@@ -1,14 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { DetailsComponent } from './details.component';
+import { RouterModule } from '@angular/router';
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
-
+ 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DetailsComponent]
+      imports: [DetailsComponent, RouterTestingModule]
     })
     .compileComponents();
     
--- src/app/housing-location/housing-location.component.spec.ts
+++ src/app/housing-location/housing-location.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HousingLocationComponent } from './housing-location.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HousingLocationComponent', () => {
   let component: HousingLocationComponent;
@@ -8,12 +9,22 @@ describe('HousingLocationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HousingLocationComponent]
+      imports: [HousingLocationComponent, RouterTestingModule]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(HousingLocationComponent);
     component = fixture.componentInstance;
+    component.housingLocation = {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: `test.com/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
+      availableUnits: 4,
+      wifi: true,
+      laundry: true
+    }
     fixture.detectChanges();
   });
 
