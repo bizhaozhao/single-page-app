87ccc8fe298af3a9789e6408c67470e3174922f4
Samuel Garratt <>
2024-03-22 21:56:24 +1300

    README and start config to run tests

---
+++ README.md
@@ -0,0 +1,26 @@
+A very simple demo Single Page Application. 
+
+* Install dependencies
+
+```
+npm install
+```
+
+* Run the single page application
+
+```
+ng run
+```
+
+* Run real service that can store and return house location data
+
+```
+docker run -v $(pwd):/home/app/serve -p 4567:4567 --rm -it registry.gitlab.com/samuel-garratt/schemaless_rest_api
+```
+
+* Run mock backend 
+
+```
+docker run -v $(pwd):/home/app/serve -p 4567:4567 --rm -it registry.gitlab.com/samuel-garratt/file_sv
+```
+
---
+++ karma.ci.conf.js
@@ -0,0 +1,33 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+	config.set({
+	  basePath: '',
+	  frameworks: ['jasmine', '@angular-devkit/build-angular', 'jasmine-matchers'],
+	  plugins: [
+		require('karma-jasmine'),
+		require('karma-chrome-launcher'),
+		require('karma-jasmine-html-reporter'),
+		require('@angular-devkit/build-angular/plugins/karma'),
+		require('karma-jasmine-matchers'),
+	  ],
+	  client: {
+		clearContext: false, // leave Jasmine Spec Runner output visible in browser
+		// See https://jasmine.github.io/setup/nodejs.html for more configuration
+		jasmine: {
+		  // Whether to fail a spec that ran no expectations
+		  failSpecWithNoExpectations: true,
+		},
+	  },
+	  reporters: ['progress', 'kjhtml'],
+	  port: 9876,
+	  colors: true,
+	  logLevel: config.LOG_INFO,
+	  autoWatch: false,
+	  browsers: ['ChromeHeadless'],
+	  singleRun: false,
+	  restartOnFileChange: true,
+	});
+  };
+  
