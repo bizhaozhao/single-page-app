a462cc700ef454bce3fa6ba63439c78ee66b1587
Samuel Garratt <>
2024-03-26 21:29:30 +1300

    Get CI test working

--- karma.ci.conf.js
+++ karma.ci.conf.js
@@ -4,13 +4,12 @@
 module.exports = function (config) {
 	config.set({
 	  basePath: '',
-	  frameworks: ['jasmine', '@angular-devkit/build-angular', 'jasmine-matchers'],
+	  frameworks: ['jasmine', '@angular-devkit/build-angular'],
 	  plugins: [
 		require('karma-jasmine'),
 		require('karma-chrome-launcher'),
 		require('karma-jasmine-html-reporter'),
-		require('@angular-devkit/build-angular/plugins/karma'),
-		require('karma-jasmine-matchers'),
+		require('@angular-devkit/build-angular/plugins/karma')
 	  ],
 	  client: {
 		clearContext: false, // leave Jasmine Spec Runner output visible in browser
