5a76eb70b3c1d4f524193d7673a4fd8f5dad65f1
Samuel Garratt <samuel.garratt@integrationqa.com>
2024-04-16 15:38:23 +1200

    Use mock backend for e2e test and add test to click and assert route

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -11,7 +11,7 @@ import { HousingLocation } from '../housing-location';
     <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
     <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    <a [attr.data-testid]="'open-' + housingLocation.id" [routerLink]="['/details', housingLocation.id]">Learn More</a>
   </section>
   `,
   styleUrl: './housing-location.component.css'
--- tests/homes.spec.ts
+++ tests/homes.spec.ts
@@ -2,10 +2,18 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Homes app', () => {
   test('should load default route', async ({page}) => {
-    await page.goto('http://localhost:4200');
+    await page.goto('/');
 
     await expect(page).toHaveTitle('Home page');
   });
+
+  test('should open a home when selecting it', async ({page}) => {
+    const id = 1;
+    const expectedName = 'House details - Acme Fresh Start Housing'
+    await page.goto('/');
+    await page.getByTestId(`open-${id}`).click();
+    await expect(page).toHaveTitle(expectedName);
+  })
 });
 
 // test('has title', async ({ page }) => {
@@ -18,9 +26,9 @@ test.describe('Homes app', () => {
 // test('get started link', async ({ page }) => {
 //   await page.goto('https://playwright.dev/');
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the get started link.
+  // await page.getByRole('link', { name: 'Get started' }).click();
 
-//   // Expects page to have a heading with the name of Installation.
+  // Expects page to have a heading with the name of Installation.
 //   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 // });
